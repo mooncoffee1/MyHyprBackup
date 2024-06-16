7bd9aedd03cfd0b5efc893f9d831324770b608e0
moonlightcoffee <mooncoffe11@gmail.com>
2024-06-16 11:15:59 +0300

    yes
    amogus

---
+++ .config/hypr/scripts/monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Command to get monitor info
+monitor_info=$(hyprctl monitors | grep -A 1 "Monitor eDP-1")
+
+# Check if 144Hz mode is active
+if echo "$monitor_info" | grep -q "1920x1080@144"; then
+    echo "Current mode is 144Hz. Switching to 60Hz."
+    # Switch to 60Hz mode
+    hyprctl keyword monitor eDP-1,1920x1080@60,auto,1
+else
+    echo "Current mode is 60Hz. Switching to 144Hz."
+    # Switch to 144Hz mode
+    hyprctl keyword monitor eDP-1,1920x1080@144,auto,1
+fi
